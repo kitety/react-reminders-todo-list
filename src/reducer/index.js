a89d01402d1052926ac92b1704f139281288f8f2
kitety <aizaizuori@foxmail.com>
2018-10-31 00:53:25 +0800

    存储cookie

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,4 +1,5 @@
 import { ADD_REMINDER, DELETE_REMINDER, CLEAR_REMINDER } from '../constants.js';
+import { bake_cookie, read_cookie, delete_cookie } from 'sfcookies'
 
 const reminder = (action) => {
   const { text, dueDate } = action;
@@ -9,17 +10,25 @@ const reminder = (action) => {
   }
 }
 // 这里要用数组 因为使用的数组遍历
-const reminders = (state = [], action = {}) => {
+const reminders = (state = read_cookie('reminders')||[], action = {}) => {
+  let reminders = null
   switch (action.type) {
     case ADD_REMINDER:
-      return [
+      reminders = [
         ...state,
         reminder(action)
       ]
+      bake_cookie('reminders', reminders)
+      return reminders
     case DELETE_REMINDER:
-      return state.filter(ele => ele.id !== action.id)
+      reminders = state.filter(ele => ele.id !== action.id)
+      bake_cookie('reminders', reminders)
+
+      return reminders
     case CLEAR_REMINDER:
-     return []
+      reminders = []
+      bake_cookie('reminders', reminders)
+      return reminders
     default:
       return state;
   }
